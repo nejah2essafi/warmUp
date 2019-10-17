92163112b7175aade99d1dc33692951ac48cc02e
nejah <nejah.essafi@hotmail.fr>
2019-10-17 13:13:21 +0200

    secend step

--- warmUp2.js
+++ warmUp2.js
@@ -2,3 +2,22 @@
 // 2-Write a function called sum that accepts two numbers as parameters, and sum them together but without suming them togther directly
 //you can only add one at each summetion, you'll need to use recursion in this.
 
+
+function gcd(a,b){
+	var c= 0;
+	if(a<b){
+		c=a
+		a=bb=c
+	}if (a===b){
+		return a
+	}if (a===0){
+		return b
+	}
+	return gcd(a-b,b)
+}
+function sum(a,b){
+	if (b===0){
+		return a
+	}
+	return 1+sum(a,b-1)
+}
