55110adfacd92216ded53693ce16f5d486de06e3
nejah <nejah.essafi@hotmail.fr>
2019-10-18 10:58:30 +0200

    third

--- warmUp3.js
+++ warmUp3.js
@@ -6,3 +6,40 @@
 // 2-write a function that takes an array as an argument and returns the element that is located in the middle of that array.
 
 // 3-Change all the numbers in the array to be multiplied by two for even indexes.
+
+
+var females= ["lina", "ruba","maroua", "nadine"] 
+var males=["hashem", "ahmed","bilel","meher"]; 
+var instructors =["saif", "youssef", "matt", "tamara","nejah"];
+
+
+function middle (array){
+	for (var i in array){
+		if (array.length%2===0){
+		return array[array.length/2]	
+		}else{
+		return array[(array.length-1)/2]	
+		}
+		
+	}
+}
+middle (instructors)
+
+//3
+
+ function each(array, func) { 
+       for (var i = 0; i < array.length; i++) { 
+             func(array[i],i); 
+       } 
+ }
+
+ function multiplied (array){
+ 		var arr = [];
+       each(array, function(ele,i){
+       	if (i%2===0)
+       	arr.push(ele*2);
+       }); 
+       return arr;
+ } 
+
+ 
