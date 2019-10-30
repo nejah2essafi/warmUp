8067f88f56fedc91db32ce83fd942f9d395460f1
nejah <nejah.essafi@hotmail.fr>
2019-10-30 10:11:26 +0100

    warm10

---
+++ warmup11/nejah2.js
@@ -0,0 +1,20 @@
+$('document').ready(function(){
+	 $('#left').on('click',function(){
+		$('#create').on('click',function(){
+			$('ul.1').append("<li> <li>")
+
+
+		})	
+	}) 
+		
+ $('#right').on('click',function(){
+		$('#create').on('click',function(){
+			$('ul.2').append("<li> <li>")
+
+
+		})	
+    })
+
+});
+
+
