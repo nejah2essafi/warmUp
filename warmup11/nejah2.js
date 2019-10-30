598180913d2b98d57ed76dfe87b6f9ca186ec3c8
nejah <nejah.essafi@hotmail.fr>
2019-10-30 12:38:13 +0100

    done

--- warmup11/nejah2.js
+++ warmup11/nejah2.js
@@ -1,19 +1,20 @@
 $('document').ready(function(){
-	 $('#left').on('click',function(){
-		$('#create').on('click',function(){
-			$('ul.1').append("<li> <li>")
+	$('#left').on('click', function(){
+		$('#create').toggleClass('1')
+	})
+	$('#right').on('click', function(){
+		$('#create').toggleClass('2')
+	})
+	if ($('#create')hasClass('1')){
+		$('#create').on('click', function(){
+		$('ul#1').append('<li> <li>')
+	})}
+		else if($('#create')hasClass('2')){
+		$('#create').on('click', function(){
+		$('ul#2').append('<li> <li>')
+	})
 
-
-		})	
-	}) 
-		
- $('#right').on('click',function(){
-		$('#create').on('click',function(){
-			$('ul.2').append("<li> <li>")
-
-
-		})	
-    })
+	}
 
 });
 
